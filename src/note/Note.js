e1ddc37cd73849215a699d154a5df2f0d80d90d5
agnes-kabaly <alicet.woods@gmail.com>
2018-08-19 16:27:27 +0200

    style + Note js + AddToDo js

---
+++ src/note/Note.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
+} from 'react-native';
+
+export default class Note extends Component {
+    render() {
+
+        return (
+            <View key={this.props.keyval} style={styles.container}>
+                <Text style={styles.noteText}>{this.props.val.date}</Text>
+                <Text style={styles.noteText}>{this.props.val.note}</Text>
+                <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+                    <Text style={styles.noteDeleteText}>D</Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        padding: 20,
+        paddingRight: 100,
+        borderBottomWidth: 2,
+        borderBottomColor: '#D8D8D8',
+    },
+    noteText: {
+        paddingLeft: 20,
+        borderLeftWidth: 10,
+        borderLeftColor: 'yellow',
+    },
+    noteDelete: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+        padding: 10,
+        top: 10,
+        bottom: 10,
+        right: 10,
+    },
+    noteDeleteText: {
+        color: 'white',
+    },
+});
--- src/todo/AddToDo.js
+++ src/todo/AddToDo.js
@@ -3,24 +3,114 @@ import {
     View,
     Text,
     StyleSheet,
+    TextInput,
+    ScrollView,
+    TouchableOpacity,
+    KeyboardAvoidingView,
 } from 'react-native';
-import {
-    Container,
-    Content,
-}from 'native-base';
+
+import Note from '../note/Note';
 
 export default class AdToDo extends React.Component {
 
+    state = {
+        noteArray: [],
+        noteText: '',
+    };
+
     render() {
 
+        let notes = this.state.noteArray.map((val, key) => {
+            return <Note key={key} keyval={key} val={val} deleteMethod={() => this.deleteNote(key)}></Note>
+        });
+
         return (
-            <View>
-                <Text>add button</Text>
-            </View>
+            <KeyboardAvoidingView behavior="padding" style={styles.wrapper}>
+                <View style={styles.container}>
+
+                    <ScrollView style={styles.scrollContainer}>
+                        {notes}
+                    </ScrollView>
+
+                    <View style={styles.footer}>
+                        <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+                            <Text style={styles.addButtonText}>+</Text>
+                        </TouchableOpacity>
+                        <TextInput style={styles.textInput}
+                                   onChangeText={(noteText) => this.setState({noteText})} value={this.noteText}
+                                   placeholder='> note'
+                                   placeholderTextColor="#0B610B"
+                                   underlineColorAndroid="transparent">
+                        </TextInput>
+                    </View>
+
+                </View>
+            </KeyboardAvoidingView>
         )
+    };
+
+    addNote() {
+        if (this.state.noteText) {
+            var d = new Date();
+            this.state.noteArray.push({
+                'date': d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() +
+                "/" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(),
+                'note': this.state.noteText});
+            this.setState({noteArray: this.state.noteArray});
+            this.setState({noteText: ''});
+        }
+    }
+
+    deleteNote(key) {
+        this.state.noteArray.splice(key, 1);
+        this.setState({noteArray: this.state.noteArray});
     }
 }
 
 const styles = StyleSheet.create ({
+    wrapper: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100,
+    },
+    footer: {
+        position: 'absolute',
+        alignItems: 'center',
+        bottom: 0,
+        right: 0,
+        left: 0,
+    },
+    addButton: {
+        backgroundColor: '#40FF00',
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        borderColor: '#0B3B0B',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: -45,
+        zIndex: 10,
+        elevation: 8,
 
+    },
+    addButtonText: {
+        color: '#ffffff',
+        fontSize: 24,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        //color: '#40FF00',
+        color: '#0B610B',
+        padding: 20,
+        paddingTop: 46,
+        backgroundColor: '#C8FE2E',
+        borderTopWidth: 2,
+        borderTopColor: 'green',
+        paddingBottom: 50,
+    },
 });
